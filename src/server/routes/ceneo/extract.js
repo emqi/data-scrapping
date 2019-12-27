898bc4c1f8e290773ed0a7ed4e980a2c2d43902b
bartek <gekarez@gmail.com>
2019-12-27 19:07:14 +0100

    Implement separate etl steps

--- src/server/routes/ceneo/extract.js
+++ src/server/routes/ceneo/extract.js
@@ -28,6 +28,28 @@ function transform(reviews) {
     return transformedReviews;
 }
 
+
+function transformTest(reviews) {
+    let transformedReviews = [];
+    reviews.forEach(review => {
+        let transformedReview = {
+            id: review.id,
+            avatar: review.avatar,
+            username: review.username.trim(),
+            rating: transformRating(review.rating),
+            upvotes: review.upvotes,
+            downvotes: review.downvotes,
+            date: review.date,
+            reviewedAfter: review.reviewedafter,
+            content: review.content,
+            reviewerBoughtProduct: review.reviewerboughtproduct,
+            productId: review.productid
+        };
+        transformedReviews.push(transformedReview);
+    });
+    return transformedReviews;
+}
+
 async function wholeProcess(res, products) {
     let dbConnector = await db.getConnection();
 
@@ -56,7 +78,7 @@ async function wholeProcess(res, products) {
                 let filteredReviews = transformedReviews.filter(function (review) {
                     return !existingReviewsIds.includes(review.reviewId.toString()) // TODO: id enough?
                 });
-                numberOfRecords += filteredReviews.length;
+                numberOfRecords += filteredReviews.length; // TODO fix calculating
 
                 filteredReviews.forEach(review => {
                     dbConnector.none('INSERT INTO reviews(id, reviewerUsername, rating, upvotes, downvotes, date, reviewedAfter, content, reviewerBoughtProduct, productId) ' +
@@ -84,18 +106,16 @@ async function wholeProcess(res, products) {
 
 module.exports = async function (req, res) {
     const phrase = req.query.phrase;
-    let pagesToSearch = req.query.pagesToSearch;
+    const pagesToSearch = req.query.pagesToSearch;
+    const mapping = req.query.mapping;
     // const phrase = "telewizor";
     // const pagesToSearch = 1;
 
-    if (pagesToSearch === '10') {
-        pagesToSearch = 1;
-        let dbConnector = await db.getConnection();
-
-
+    const dbConnector = await db.getConnection();
+    // GET RESULT REVIEWS
+    if (mapping === '11') {
         dbConnector.any("select * from reviews left join products on productId = products.id")
             .then(function (allReviews) {
-                console.log(allReviews);
                 return res.json(allReviews);
             })
             .catch(function () {
@@ -104,8 +124,117 @@ module.exports = async function (req, res) {
             )
     }
 
-    const data = await extract(phrase, pagesToSearch);
-    await wholeProcess(res, data);
+
+    // ETL PROCESS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if (mapping === '123') {
+        const data = await extract(phrase, pagesToSearch);
+        await wholeProcess(res, data);
+    }
+    // EXTRACT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if (mapping === '1') {
+        // if (mapping === 'EXTRACT') {
+        let TODO_RENAME = 0;
+
+        const products = await extract(phrase, pagesToSearch);
+        // const products = getTestProductWithOpinions();
+        let count = 0;
+        let reviewsCount = data.forEach(product => count += product.reviews.length);
+        console.log("number of reviews: " + reviewsCount);
+        products.forEach(product => {
+            dbConnector.none('INSERT INTO products(id, name, description, rating, price) ' +
+                'VALUES(${id}, ${name}, ${description}, ${rating}, ${price})', {
+                id: product.id,
+                name: product.name,
+                description: product.description,
+                rating: product.rating,
+                price: product.price
+            }).catch(function () {
+                return res.json("Error");
+            });
+
+            TODO_RENAME += product.reviews.length;
+            product.reviews.forEach(review => {
+                dbConnector.none('INSERT INTO reviews_extract(id, avatar, username, rating, upvotes, downvotes, date, reviewedAfter, content, reviewerBoughtProduct, productId) ' +
+                    'VALUES(${id}, ${avatar}, ${username}, ${rating}, ${upvotes}, ${downvotes}, ${date}, ${reviewedAfter}, ${content}, ${reviewerBoughtProduct}, ${productId})', {
+                    id: review.reviewId,
+                    avatar: review.reviewer.avatar,
+                    username: review.reviewer.username,
+                    rating: review.rating,
+                    upvotes: review.usefulness.upvotes,
+                    downvotes: review.usefulness.downvotes,
+                    date: review.date,
+                    reviewedAfter: review.reviewedAfter,
+                    content: review.text,
+                    reviewerBoughtProduct: review.didUserBuyTheProduct,
+                    productId: product.id
+                }).catch(function () {
+                    return res.json("Error");
+                });
+            });
+        });
+        return res.json(TODO_RENAME);
+    }
+
+    // TRANSFORM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    else if (mapping === '2') {
+        // else if (mapping === 'TRANSFORM') {
+        dbConnector.any("SELECT * FROM reviews_extract")
+            .then(function (reviews) {
+                const transformedReviews = transformTest(reviews);
+                transformedReviews.forEach(review => {
+                    dbConnector.none('INSERT INTO reviews_transform(id, avatar, username, rating, upvotes, downvotes, date, reviewedAfter, content, reviewerBoughtProduct, productId) ' +
+                        'VALUES(${id}, ${avatar}, ${username}, ${rating}, ${upvotes}, ${downvotes}, ${date}, ${reviewedAfter}, ${content}, ${reviewerBoughtProduct}, ${productId})', {
+                        id: review.id,
+                        avatar: review.avatar,
+                        username: review.username,
+                        rating: review.rating,
+                        upvotes: review.upvotes,
+                        downvotes: review.downvotes,
+                        date: review.date,
+                        reviewedAfter: review.reviewedAfter,
+                        content: review.content,
+                        reviewerBoughtProduct: review.reviewerBoughtProduct,
+                        productId: review.productId
+                    }).catch(function () {
+                        return res.json("Error");
+                    });
+                });
+
+                dbConnector.none('DELETE FROM reviews_extract')
+                    .then(() => console.log('DELETE FROM reviews_extract'));
+                return res.json(reviews.length);
+            });
+    } else if (mapping === '3') {
+        // } else if (mapping === 'LOAD') {
+
+        dbConnector.any("SELECT * FROM reviews_transform")
+            .then(function (transformedReviews) {
+                // TODO: jezeli mozna wykonac load wiele razy to trzeba filtrowac reviews zeby nie duplikowac
+                transformedReviews.forEach(review => {
+                    dbConnector.none('INSERT INTO reviews(id, reviewerUsername, rating, upvotes, downvotes, date, reviewedAfter, content, reviewerBoughtProduct, productId) ' +
+                        'VALUES(${id}, ${reviewerUsername}, ${rating}, ${upvotes}, ${downvotes}, ${date}, ${reviewedAfter}, ${content}, ${reviewerBoughtProduct}, ${productId})', {
+                        id: review.id,
+                        reviewerUsername: review.username,
+                        rating: review.rating,
+                        upvotes: review.upvotes,
+                        downvotes: review.downvotes,
+                        date: review.date,
+                        reviewedAfter: review.reviewedafter,
+                        content: review.content,
+                        reviewerBoughtProduct: review.reviewerboughtproduct,
+                        productId: review.productid
+                    });
+                });
+                dbConnector.none('DELETE FROM reviews_transform')
+                    .then(() => console.log('DELETE FROM reviews_transform'));
+                return res.json(transformedReviews.length);
+            })
+            .catch(function () {
+                    console.log("Error");
+                    // return res.json("Error");
+                }
+            );
+    }
 
 
 };
